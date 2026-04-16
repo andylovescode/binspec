b22e4ff40189bc80d242d27d74a2285148aed01f
andylovescode <andytk2010@gmail.com>
2026-04-16 10:45:13 -0700

    Example: Use bitmasks

--- example/spec.gen.ts
+++ example/spec.gen.ts
@@ -1,103 +1,211 @@
-type IO = { ptr: number; buffer: Uint8Array; dataView: DataView; littleEndian: boolean }
+type IO = {
+	ptr: number
+	buffer: Uint8Array
+	dataView: DataView
+	littleEndian: boolean
+}
 function createIOContext(buffer: Uint8Array = new Uint8Array(10000)): IO {
-return { ptr: 0, buffer, dataView: new DataView(buffer.buffer), littleEndian: true }
+	return {
+		ptr: 0,
+		buffer,
+		dataView: new DataView(buffer.buffer),
+		littleEndian: true,
+	}
 }
 
 /*
 	Ping
 */
-export type Ping = PingUnix | PingSecondsSince2000;
+export type Ping = PingUnix | PingSecondsSince2000
 export function parsePing(parseInput: IO | Uint8Array): Ping {
-const context = parseInput instanceof Uint8Array ? createIOContext(parseInput) : parseInput
-const sign = parseUnsigned8(context)
-if (sign === 0) {
-return parsePingUnix(context)
+	const context = parseInput instanceof Uint8Array
+		? createIOContext(parseInput)
+		: parseInput
+	const sign = parseUnsigned8(context)
+	if (sign === 0) {
+		return parsePingUnix(context)
+	}
+	if (sign === 420) {
+		return parsePingSecondsSince2000(context)
+	}
+	throw new Error("invalid variant")
 }
-if (sign === 420) {
-return parsePingSecondsSince2000(context)
-}
-throw new Error('invalid variant');
-}
-export function writePing(val: Ping, context: IO = createIOContext()): Uint8Array {
-if (val.type === "PingUnix") {
-writeUnsigned8(0,context)
-writePingUnix(val,context)
-}
-else if (val.type === "PingSecondsSince2000") {
-writeUnsigned8(420,context)
-writePingSecondsSince2000(val,context)
-}
-else {
-throw new Error('invalid variant')
-}
-return context.buffer.slice(0,context.ptr)
+export function writePing(
+	val: Ping,
+	context: IO = createIOContext(),
+): Uint8Array {
+	if (val.type === "PingUnix") {
+		writeUnsigned8(0, context)
+		writePingUnix(val, context)
+	} else if (val.type === "PingSecondsSince2000") {
+		writeUnsigned8(420, context)
+		writePingSecondsSince2000(val, context)
+	} else {
+		throw new Error("invalid variant")
+	}
+	return context.buffer.slice(0, context.ptr)
 }
 
 /*
 	Unsigned8
 */
-export type Unsigned8 = number;
+export type Unsigned8 = number
 export function parseUnsigned8(parseInput: IO | Uint8Array): Unsigned8 {
-const context = parseInput instanceof Uint8Array ? createIOContext(parseInput) : parseInput
-return context.dataView.getUint8((context.ptr++))
+	const context = parseInput instanceof Uint8Array
+		? createIOContext(parseInput)
+		: parseInput
+	return context.dataView.getUint8(context.ptr++)
 }
-export function writeUnsigned8(val: Unsigned8, context: IO = createIOContext()): Uint8Array {
-context.dataView.setUint8((context.ptr++),val)
-return context.buffer.slice(0,context.ptr)
+export function writeUnsigned8(
+	val: Unsigned8,
+	context: IO = createIOContext(),
+): Uint8Array {
+	context.dataView.setUint8(context.ptr++, val)
+	return context.buffer.slice(0, context.ptr)
 }
 
 /*
 	PingUnix
 */
-export type PingUnix = {timestampNs:number,type:"PingUnix"};
-export function parsePingUnix(parseInput: IO | Uint8Array): PingUnix {
-const context = parseInput instanceof Uint8Array ? createIOContext(parseInput) : parseInput
-const [timestampNs] = [parseUnsigned64(context)]
-return {timestampNs,type:"PingUnix"}
+export type PingUnix = {
+	timestampNs: Unsigned64
+	bmask: TestingBitmask
+	type: "PingUnix"
 }
-export function writePingUnix(val: PingUnix, context: IO = createIOContext()): Uint8Array {
-if (val.type !== "PingUnix") throw new Error("Expected an object with type 'PingUnix'");
-writeUnsigned64(val.timestampNs, context)
-return context.buffer.slice(0,context.ptr)
+export function parsePingUnix(parseInput: IO | Uint8Array): PingUnix {
+	const context = parseInput instanceof Uint8Array
+		? createIOContext(parseInput)
+		: parseInput
+	const [timestampNs, bmask] = [
+		parseUnsigned64(context),
+		parseTestingBitmask(context),
+	]
+	return { timestampNs, bmask, type: "PingUnix" }
+}
+export function writePingUnix(
+	val: PingUnix,
+	context: IO = createIOContext(),
+): Uint8Array {
+	if (val.type !== "PingUnix") {
+		throw new Error("Expected an object with type 'PingUnix'")
+	}
+	writeUnsigned64(val.timestampNs, context)
+	writeTestingBitmask(val.bmask, context)
+	return context.buffer.slice(0, context.ptr)
 }
 
 /*
 	Unsigned64
 */
-export type Unsigned64 = number;
+export type Unsigned64 = number
 export function parseUnsigned64(parseInput: IO | Uint8Array): Unsigned64 {
-const context = parseInput instanceof Uint8Array ? createIOContext(parseInput) : parseInput
-return Number(context.dataView.getBigUint64(((context.ptr += 8) - 8), context.littleEndian))
+	const context = parseInput instanceof Uint8Array
+		? createIOContext(parseInput)
+		: parseInput
+	return Number(
+		context.dataView.getBigUint64((context.ptr += 8) - 8, context.littleEndian),
+	)
 }
-export function writeUnsigned64(val: Unsigned64, context: IO = createIOContext()): Uint8Array {
-context.dataView.setBigUint64(((context.ptr += 8) - 8),BigInt(val), context.littleEndian)
-return context.buffer.slice(0,context.ptr)
+export function writeUnsigned64(
+	val: Unsigned64,
+	context: IO = createIOContext(),
+): Uint8Array {
+	context.dataView.setBigUint64(
+		(context.ptr += 8) - 8,
+		BigInt(val),
+		context.littleEndian,
+	)
+	return context.buffer.slice(0, context.ptr)
+}
+
+/*
+	TestingBitmask
+*/
+export type TestingBitmask = {
+	a: boolean
+	b: boolean
+	c: boolean
+	d: boolean
+	e: boolean
+	f: boolean
+	g: boolean
+	h: boolean
+}
+export function parseTestingBitmask(
+	parseInput: IO | Uint8Array,
+): TestingBitmask {
+	const context = parseInput instanceof Uint8Array
+		? createIOContext(parseInput)
+		: parseInput
+	const num = parseUnsigned8(context)
+	return {
+		a: (num & 1) === 1,
+		b: (num & 2) === 2,
+		c: (num & 4) === 4,
+		d: (num & 8) === 8,
+		e: (num & 16) === 16,
+		f: (num & 32) === 32,
+		g: (num & 64) === 64,
+		h: (num & 128) === 128,
+	}
+}
+export function writeTestingBitmask(
+	val: TestingBitmask,
+	context: IO = createIOContext(),
+): Uint8Array {
+	writeUnsigned8(
+		0 | (val.a ? 1 : 0) | (val.b ? 2 : 0) | (val.c ? 4 : 0) | (val.d ? 8 : 0) |
+			(val.e ? 16 : 0) | (val.f ? 32 : 0) | (val.g ? 64 : 0) |
+			(val.h ? 128 : 0),
+		context,
+	)
+	return context.buffer.slice(0, context.ptr)
 }
 
 /*
 	PingSecondsSince2000
 */
-export type PingSecondsSince2000 = {secondsSince2000:number,type:"PingSecondsSince2000"};
-export function parsePingSecondsSince2000(parseInput: IO | Uint8Array): PingSecondsSince2000 {
-const context = parseInput instanceof Uint8Array ? createIOContext(parseInput) : parseInput
-const [secondsSince2000] = [parseFloat64(context)]
-return {secondsSince2000,type:"PingSecondsSince2000"}
+export type PingSecondsSince2000 = {
+	secondsSince2000: Float64
+	type: "PingSecondsSince2000"
 }
-export function writePingSecondsSince2000(val: PingSecondsSince2000, context: IO = createIOContext()): Uint8Array {
-if (val.type !== "PingSecondsSince2000") throw new Error("Expected an object with type 'PingSecondsSince2000'");
-writeFloat64(val.secondsSince2000, context)
-return context.buffer.slice(0,context.ptr)
+export function parsePingSecondsSince2000(
+	parseInput: IO | Uint8Array,
+): PingSecondsSince2000 {
+	const context = parseInput instanceof Uint8Array
+		? createIOContext(parseInput)
+		: parseInput
+	const [secondsSince2000] = [parseFloat64(context)]
+	return { secondsSince2000, type: "PingSecondsSince2000" }
+}
+export function writePingSecondsSince2000(
+	val: PingSecondsSince2000,
+	context: IO = createIOContext(),
+): Uint8Array {
+	if (val.type !== "PingSecondsSince2000") {
+		throw new Error("Expected an object with type 'PingSecondsSince2000'")
+	}
+	writeFloat64(val.secondsSince2000, context)
+	return context.buffer.slice(0, context.ptr)
 }
 
 /*
 	Float64
 */
-export type Float64 = number;
+export type Float64 = number
 export function parseFloat64(parseInput: IO | Uint8Array): Float64 {
-const context = parseInput instanceof Uint8Array ? createIOContext(parseInput) : parseInput
-return context.dataView.getFloat64(((context.ptr += 8) - 8), context.littleEndian)
+	const context = parseInput instanceof Uint8Array
+		? createIOContext(parseInput)
+		: parseInput
+	return context.dataView.getFloat64(
+		(context.ptr += 8) - 8,
+		context.littleEndian,
+	)
 }
-export function writeFloat64(val: Float64, context: IO = createIOContext()): Uint8Array {
-context.dataView.setFloat64(((context.ptr += 8) - 8),val, context.littleEndian)
-return context.buffer.slice(0,context.ptr)
+export function writeFloat64(
+	val: Float64,
+	context: IO = createIOContext(),
+): Uint8Array {
+	context.dataView.setFloat64((context.ptr += 8) - 8, val, context.littleEndian)
+	return context.buffer.slice(0, context.ptr)
 }
--- example/spec.ts
+++ example/spec.ts
@@ -1,7 +1,18 @@
-import { enumerated, f64, struct, u64 } from "../src/types.ts"
+import { bitmask, enumerated, f64, struct, u64 } from "../src/types.ts"
+
+const testBitmask = bitmask("TestingBitmask")
+	.field("a", 0b00000001)
+	.field("b", 0b00000010)
+	.field("c", 0b00000100)
+	.field("d", 0b00001000)
+	.field("e", 0b00010000)
+	.field("f", 0b00100000)
+	.field("g", 0b01000000)
+	.field("h", 0b10000000)
 
 const pingUnix = struct("PingUnix")
 	.field("timestampNs", u64())
+	.field("bmask", testBitmask)
 
 // don't do this in production, this is a dumb way of notating time
 const pingSecondsSince2000 = struct("PingSecondsSince2000")
